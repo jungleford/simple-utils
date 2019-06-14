4fcd0e028d5715851e14c896ce925daa4c2f5102
jungleford <jungleford@gmail.com>
2019-06-14 11:14:44 +0800

    Remove unnecessary babel plugins for production mode, since all codes in lib/js/ were translated by babel.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,37 +3,12 @@ import webpack from 'webpack';
 
 const root = path.resolve(__dirname);
 
-const clientInclude = [
-  path.join(root, 'lib', 'js')
-];
-
-const babelQuery = {
-  presets: [
-    'babel-preset-es2015',
-    'babel-preset-stage-0'
-  ]
-};
-
 export default {
   mode: 'production',
-  entry: ['babel-polyfill', path.join(root, 'lib', 'js', 'index.js')],
+  entry: [path.join(root, 'lib', 'js', 'index.js')],
   output: {
     path: path.join(root, 'dist', 'js'),
     filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        query: babelQuery,
-        include: clientInclude,
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolveLoader: {
-    moduleExtensions: ['-loader']
   },
   plugins: [
     new webpack.ProvidePlugin({
